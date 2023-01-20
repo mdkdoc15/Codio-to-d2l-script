c52a19843fb87a0494db7bb36d3bc5792f2d7014
Matthew Kight <70612402+mdkdoc15@users.noreply.github.com>
2023-01-20 11:13:19 -0500

    Cleans data and writes to file.

--- grade_transfer.py
+++ grade_transfer.py
@@ -6,6 +6,7 @@ Goal: Convert CSV from codio to useable D2L CSV
 
 import csv
 
+
 def read_csv_file(filename):
     """
     Takes a filename denoting a CSV formatted file.
@@ -27,13 +28,13 @@ def write_csv_file(filename, data):
 
     with open(filename, 'w', newline='') as csvfile:
         fieldnames = [field for field in data[0]]
-        fieldnames.sort()
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for record in data:
             writer.writerow(record)
 
-def clean_data(data):
+
+def clean_data(data, project_name):
     """
     Takes in CSV data and converts to D2L desierable form
     :param data: CSV data
@@ -46,7 +47,7 @@ def clean_data(data):
     # New headers
     end_of_line_header = "End-of-line indicator"
     end_of_line_value = "#"
-    points_header = "ProjectXX Points Grade" #TODO Update to variable
+    points_header = f"{project_name} Points Grade" #TODO Update to variable
     username_header = "Username"
     new_data = []
     for i, student in enumerate(data):
@@ -55,24 +56,31 @@ def clean_data(data):
         new_data.append({})
         # Turn email to Username column
         # Username is net id, AKA, everything before at sign
-        new_data[-1][username_header] = data[i][email_header][: (data[i][email_header].find("@") -1) ]
+        new_data[-1][username_header] = data[i][email_header][: data[i][email_header].find("@") ]
         # Update column name of the grades
         new_data[-1][points_header] = data[i][final_grade_header]
         # Add End of line info
         new_data[-1][end_of_line_header] = end_of_line_value
-
-    for i, val in enumerate(new_data):
-        if i > 5:
-            break
-        print(val)
-
     return new_data
 
+def get_d2l_filename(orig_file_path):
+    '''
+    Get updated file name from the path passed in
+
+    :param orig_file_path: original path that we are parsing
+    :return: file name up until the first '.' char
+    '''
+    updated_filename = orig_file_path[: orig_file_path.find(".")]
+    return f"{updated_filename}_d2l.csv"
 
 def main():
-    file_path = "cse-331-ss23_project00-s23_1673928366429.csv"
+    file_path = "projectXX.from_codio.csv"
+    d2l_file_name = get_d2l_filename(file_path)
+    project_name = d2l_file_name[:-8] # Strip the _d2l.csv from the file name
     data = read_csv_file(file_path)
-    clean_data(data)
+    d2l_data = clean_data(data, project_name)
+
+    write_csv_file(d2l_file_name, d2l_data)
 
 
 if __name__ == '__main__':
